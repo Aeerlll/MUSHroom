aa59a9e31b5516f1d7571147b3017114be2eed46
Aeerlll <109431687+Aeerlll@users.noreply.github.com>
2025-06-03 13:41:35 +0300

    Gun.cs

--- MUSHroom game/Assets/Scripts/Gun.cs
+++ MUSHroom game/Assets/Scripts/Gun.cs
@@ -1,21 +1,29 @@
-﻿using System.Collections;
+using System.Collections;
 using UnityEngine;
 
+// Класс Gun отвечает за поведение оружия (стрельбу)
 public class Gun : MonoBehaviour {
 
-    public Transform muzzle;
-    public Shooting projectile;
-    public float msBeetweenShots = 100;
-    public float muzzleVelocity = 35;
+    public Transform muzzle;          // Точка, откуда будут вылетать снаряды
+    public Shooting projectile;        // Префаб снаряда для стрельбы
+    public float msBeetweenShots = 100;// Время между выстрелами в миллисекундах
+    public float muzzleVelocity = 35;  // Начальная скорость снаряда
 
-    float nextShotTime;
+    float nextShotTime;                // Время, когда можно будет сделать следующий выстрел
 
+    // Метод Shoot вызывается для совершения выстрела
     public void Shoot()
     {
+        // Проверяем, можно ли стрелять (прошло ли достаточно времени с последнего выстрела)
         if (Time.time > nextShotTime)
         {
-            nextShotTime = Time.time + msBeetweenShots / 1000;
+            // Обновляем время следующего возможного выстрела
+            nextShotTime = Time.time + msBeetweenShots / 1000; // делим на 1000 для перевода мс в секунды
+            
+            // Создаем новый экземпляр снаряда в позиции дула оружия
             Shooting newProjectile = Instantiate(projectile, muzzle.position, muzzle.rotation) as Shooting;
+            
+            // Устанавливаем скорость созданного снаряда
             newProjectile.SetSpeed(muzzleVelocity);
         }
     }
